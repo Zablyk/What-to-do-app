862bbf88f980b48bb19d44feba5f388067505ff5
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-06-04 18:45:29 -0400

    added functionality to delete iten using deleteItemHandler and passing properties throught todo-list-item and todo-list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header/app-header';
 import SearchPanel from '../search-panel/search-panel';
@@ -7,14 +7,31 @@ import TodoList from '../todo-list/todo-list';
 import './app.css';
 
 
-const App = () => {
+export default class App extends Component {
 
-    const todoData = [
-      { label: 'Drink Coffee', important: false, id: 1 },
-      { label: 'Make Awesome App', important: true, id: 2 },
-      { label: 'Have a lunch', important: false, id: 3 }
-    ];
-  
+  state = {
+      todoData: [
+        { label: 'Drink Coffee', important: false, id: 1 },
+        { label: 'Make Awesome App', important: true, id: 2 },
+        { label: 'Have a lunch', important: false, id: 3 }
+      ]
+    };
+
+  deletedItemHandler = (id) => {
+      this.setState(({todoData}) => {
+        const idx = todoData.findIndex((el) => el.id === id);
+        const newArray = [
+          ...todoData.slice( 0, idx ),
+          ...todoData.slice( idx + 1 ),
+        ];
+
+        return {
+          todoData: newArray
+        };
+    });
+  };  
+
+  render() {
     return (
       <div className="todo-app">
         <AppHeader toDo={1} done={3} />
@@ -23,9 +40,10 @@ const App = () => {
           <ItemStatusFilter />
         </div>
   
-        <TodoList todos={todoData} />
+        <TodoList
+          todos={ this.state.todoData }
+          onDeleted={this.deletedItemHandler} />
       </div>
     );
+    };
   };
-
-export default App;
