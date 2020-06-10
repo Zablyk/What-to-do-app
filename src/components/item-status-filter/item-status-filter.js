d6029d06af5565fd4b14b1ffab790d80bf176ba1
Zablyk <zablotsky@gmail.com>
2020-06-09 23:32:19 -0400

    changing a code for functionality for all/active/done bottons

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -3,7 +3,19 @@ import React, { Component } from 'react';
 import './item-status-filter.css';
 
 export default class ItemStatusFilter extends Component {
+  bottons = [
+    {name: 'all', label: 'All'},
+    {name: 'active', label: 'Active'},
+    {name: 'done', label: 'Done'}
+  ];
+  
   render() {
+    const buttons = this.bottons.map((name, label) => {
+      return (
+        <button type="button"
+      className="btn btn-info">{label}</button>
+      );
+    });
     return (
       <div className="btn-group">
         <button type="button"
